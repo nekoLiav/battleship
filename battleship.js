a36b3c50e9403db5fdad79f7f9b966b115923af5
Liav <oneandonlyliav@gmail.com>
2022-03-14 14:05:01 -0400

    added horizontal placement support and relevant tests

--- battleship.js
+++ battleship.js
@@ -1,11 +1,18 @@
 const ship = (shipLength) => {
+  // array consists of number of indexes equal to ship parameter
+  // every index contains a zero to start
   const shipArray = [];
   for (i = 0; i < shipLength; i++) {
     shipArray.push(0);
   };
+  // flips the index to a one upon hit detection
+  // n parameter indicates index hit
+  // - 1 is for zero based indexing correction
   const hit = (n) => {
     shipArray[n - 1] = 1;
   };
+  // sums shipArray, compares sum to array length
+  // if array length is equal, this implies it is filled with ones
   const isSunk = () => {
     const shipHitsArray = shipArray.reduce(
       (prev, cur) => prev + cur);
@@ -17,6 +24,8 @@ const ship = (shipLength) => {
 };
 
 const gameBoard = () => {
+  // array consists of 100 sub arrays
+  // each 10 sub arrays contains numbers 0-9
   const boardArray = [];
   for (i = 0, n = 0; i < 100; i++, n++) {
     if (n === 10) {
@@ -32,21 +41,32 @@ const gameBoard = () => {
     patrolboat: ship(2)
   };
   const placeShip = (shipType, orientation, location) => {
-    if (shipType.shipArray.length >= 5 && location > 59) {
-      throw 'Out of bounds!'
-    } else if (shipType.shipArray.length >= 4 && location > 69) {
-      throw 'Out of bounds!'
-    } else if (shipType.shipArray.length >= 3 && location > 79) {
-      throw 'Out of bounds!'
-    } else if (shipType.shipArray.length >= 2 && location > 89) {
-      throw 'Out of bounds!'
-    } 
+    // raw location checks vertical orientation error, 
+    // boardArray[location][index] checks horizontal orientation error
+    if (shipType.shipArray.length >= 5 && 
+      location > 59 || boardArray[location][0] >= 6) {
+      throw 'Out of bounds!';
+    } else if (shipType.shipArray.length >= 4 && 
+      location > 69 || boardArray[location][0] >= 7) {
+      throw 'Out of bounds!';
+    } else if (shipType.shipArray.length >= 3 && 
+      location > 79 || boardArray[location][0] >= 8) {
+      throw 'Out of bounds!';
+    } else if (shipType.shipArray.length >= 2 && 
+      location > 89 || boardArray[location][0] >= 9) {
+      throw 'Out of bounds!';
+    };
+    // orientation of 0 indicates vertical placement
     if (orientation === 0) {
-      let n = -10;
-      for (i = 0; i < shipType.shipArray.length; i++) {
-          n += 10;
+      for (i = 0, n = 0; i < shipType.shipArray.length; i++, n += 10) {
           boardArray[location + n].push(shipType);
       };
+    };
+    // orientation of 1 indicates horizontal placement
+    if (orientation === 1) {
+      for (i = 0; i < shipType.shipArray.length; i++) {
+        boardArray[location + i].push(shipType);
+      }
     };
   };
   return { boardArray, ships, placeShip }
--- battleship.test.js
+++ battleship.test.js
@@ -19,10 +19,28 @@ test('isSunk method returns "deth" when shipArray contains all 1s', () => {
   expect(testBoard.ships.destroyer.isSunk()).toBe('deth');
 });
 
-test('placeShip function adds ship information to indicated array index', () => {
+test('placeShip function adds ship information to indicated beginning index with vertical orientation', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
   expect(testBoard.boardArray[0]).toEqual([0, testBoard.ships.destroyer]);
+});
+
+test('placeShip function adds ship information to indicated beginning index with horizontal orientation', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
+  expect(testBoard.boardArray[0]).toEqual([0, testBoard.ships.destroyer]);
+});
+
+test('placeShip function adds ship information to all indexes it should occupy with horizontal orientation (1)', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
+  expect(testBoard.boardArray[1]).toEqual([1, testBoard.ships.destroyer]);
+});
+
+test('placeShip function adds ship information to all indexes it should occupy with horizontal orientation(2)', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
+  expect(testBoard.boardArray[2]).toEqual([2, testBoard.ships.destroyer]);
 });
 
 test('placeShip function adds ship information to all indexes it should occupy with vertical orientation', () => {
@@ -38,10 +56,24 @@ test('placeShip throws an error if a carrier would be placed out of bounds in ve
   }).toThrow();
 });
 
+test('placeShip throws an error if a carrier would be placed out of bounds in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  expect(() => {
+    testBoard.placeShip(testBoard.ships.carrier, 1, 6);
+  }).toThrow();
+});
+
 test('placeShip throws an error if a battleship would be placed out of bounds in vertical orientation', () => {
   const testBoard = gameBoard();
   expect(() => {
     testBoard.placeShip(testBoard.ships.battleship, 0, 70);
+  }).toThrow();
+});
+
+test('placeShip throws an error if a battleship would be placed out of bounds in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  expect(() => {
+    testBoard.placeShip(testBoard.ships.battleship, 1, 7);
   }).toThrow();
 });
 
@@ -52,6 +84,13 @@ test('placeShip throws an error if a destroyer would be placed out of bounds in 
   }).toThrow();
 });
 
+test('placeShip throws an error if a destroyer would be placed out of bounds in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  expect(() => {
+    testBoard.placeShip(testBoard.ships.destroyer, 1, 7);
+  }).toThrow();
+});
+
 test('placeShip throws an error if a submarine would be placed out of bounds in vertical orientation', () => {
   const testBoard = gameBoard();
   expect(() => {
@@ -59,9 +98,23 @@ test('placeShip throws an error if a submarine would be placed out of bounds in 
   }).toThrow();
 });
 
+test('placeShip throws an error if a submarine would be placed out of bounds in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  expect(() => {
+    testBoard.placeShip(testBoard.ships.submarine, 1, 8);
+  }).toThrow();
+});
+
 test('placeShip throws an error if a patrol boat would be placed out of bounds in vertical orientation', () => {
   const testBoard = gameBoard();
   expect(() => {
     testBoard.placeShip(testBoard.ships.patrolboat, 0, 90);
+  }).toThrow();
+});
+
+test('placeShip throws an error if a patrol boat would be placed out of bounds in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  expect (() => {
+    testBoard.placeShip(testBoard.ships.patrolboat, 1, 9);
   }).toThrow();
 });

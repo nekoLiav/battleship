033ef6c05ddccac131e6ae53a53e7be717ff2ddd
Liav <oneandonlyliav@gmail.com>
2022-03-15 13:19:05 -0400

    added full ship hit/sunk detection and relevant tests

--- battleship.js
+++ battleship.js
@@ -1,18 +1,16 @@
-const ship = (shipLength) => {
-  // array consists of number of indexes equal to ship parameter
-  // every index contains a zero to start
+const ship = (shipLength, shipType) => {
+  const type = shipType;
+  // Populate an array with 0s for hit/sunk detection
   const shipArray = [];
   for (i = 0; i < shipLength; i++) {
     shipArray.push(0);
   }
-  // flips the index to a one upon hit detection
-  // n parameter indicates index hit
-  // - 1 is for zero based indexing correction
+  // Flip the array 0s to 1s if hit is detected
   const hit = (n) => {
-    shipArray[n - 1] = 1;
+    shipArray[n] = 1;
   };
-  // sums shipArray, compares sum to array length
-  // if array length is equal, this implies it is filled with ones
+  // If the length of the array is equal to the sum of its elements,
+  // this implies that it is filled with 1s, or fully 'hit'
   const isSunk = () => {
     const shipHitsArray = shipArray.reduce(
       (prev, cur) => prev + cur);
@@ -20,64 +18,77 @@ const ship = (shipLength) => {
         return 'deth';
       }
   };
-  return { shipArray, hit, isSunk }
+  return { type, shipArray, hit, isSunk }
 };
 
 const gameBoard = () => {
-  // array consists of 100 sub arrays
-  // each 10 sub arrays contains numbers 0-9
   const boardArray = [];
+  // Populate boardArray with 3 values: x, y coordinates,
+  // and 0s to flip for hit/miss detection
   for (i = 0, x = 0, y = 0; i < 100; i++, x++) {
     if (x === 10) {
       x = 0;
       y += 1;
     }
-    boardArray.push([x, y]);
+    boardArray.push([x, y, 0]);
   }
+  // Ship factory goes brrrrrrrrrrrrr
   const ships = {
-    carrier: ship(5),
-    battleship: ship(4),
-    destroyer: ship(3),
-    submarine: ship(3),
-    patrolboat: ship(2)
+    carrier: ship(5, 'carrier'),
+    battleship: ship(4, 'battleship'),
+    destroyer: ship(3, 'destroyer'),
+    submarine: ship(3, 'submarine'),
+    patrolboat: ship(2, 'patrol boat')
   };
-  const placeShip = (shipType, orientation, location) => {
-    // raw location checks vertical orientation error, 
-    // boardArray[location][index] checks horizontal orientation error
-    if (shipType.shipArray.length >= 5 && 
+  // This if/else block detects invalid/out of bounds ship placements
+  const placeShip = (ship, orientation, location) => {
+    if (ship.shipArray.length >= 5 && 
       boardArray[location][0] >= 6 || boardArray[location][1] >= 6) {
       throw 'Out of bounds!';
-    } else if (shipType.shipArray.length >= 4 && 
+    } else if (ship.shipArray.length >= 4 && 
       boardArray[location][0] >= 7 || boardArray[location][1] >= 7) {
       throw 'Out of bounds!';
-    } else if (shipType.shipArray.length >= 3 && 
+    } else if (ship.shipArray.length >= 3 && 
       boardArray[location][0] >= 8 || boardArray[location][1] >= 8) {
       throw 'Out of bounds!';
-    } else if (shipType.shipArray.length >= 2 && 
+    } else if (ship.shipArray.length >= 2 && 
       boardArray[location][0] >= 9 || boardArray[location][1] >= 9) {
       throw 'Out of bounds!';
     }
-    // orientation of 0 indicates vertical placement
+    // Orientation of 0 indicates vertical placement
     if (orientation === 0) {
-      for (i = 0, n = 0; i < shipType.shipArray.length; i++, n += 10) {
-          boardArray[location + n].push(shipType);
+      for (i = 0, n = 0; i < ship.shipArray.length; i++, n += 10) {
+          boardArray[location + n].push(ship.type, i);
       }
     }
-    // orientation of 1 indicates horizontal placement
+    // Orientation of 1 indicates horizontal placement
     if (orientation === 1) {
-      for (i = 0; i < shipType.shipArray.length; i++) {
-        boardArray[location + i].push(shipType);
+      for (i = 0; i < ship.shipArray.length; i++) {
+        boardArray[location + i].push(ship.type, i);
       }
     }
   };
-  // accepts two coordinates and targets the associated boardArray index
+  // Target boardArray index based on x, y parameters
   const receiveAttack = (x, y) => {
     let coords = [x, y];
     for (i = 0; i < boardArray.length; i++) {
       if (boardArray[i][0] == coords[0] && boardArray[i][1] == coords[1]) {
-        return(i);
+        boardArray[i][2] = 1;
+        // Clumsy way of detecting which type of ship was hit, need to improve this
+        // Will probably end up using a reverse lookup within ships object
+        if (boardArray[i][3] === 'carrier') {
+          ships.carrier.hit(boardArray[i][4]);
+        } else if (boardArray[i][3] === 'battleship') {
+          ships.battleship.hit(boardArray[i][4]);
+        } else if (boardArray[i][3] === 'destroyer') {
+          ships.destroyer.hit(boardArray[i][4]);
+        } else if (boardArray[i][3] === 'submarine') {
+          ships.submarine.hit(boardArray[i][4]);
+        } else if (boardArray[i][3] === 'patrol boat') {
+          ships.patrolboat.hit(boardArray[i][4]);
+        }
       }
-    }
+    } 
   };
   return { boardArray, ships, placeShip, receiveAttack }
 };
--- battleship.test.js
+++ battleship.test.js
@@ -5,59 +5,81 @@ test('shipArray is declared with quantity of indexes equivalent to argument, and
   expect(testBoard.ships.destroyer.shipArray).toEqual([0, 0, 0]);
 });
 
-test('hit method affects shipArray correctly by flipping the index of (argument - 1) to a 1 (the - 1 is to correct for zero based indexing)', () => {
+test('hit method affects shipArray correctly by flipping the index of parameter to a 1', () => {
   const testBoard = gameBoard();
-  testBoard.ships.destroyer.hit(1);
+  testBoard.ships.destroyer.hit(0);
   expect(testBoard.ships.destroyer.shipArray).toEqual([1, 0, 0]);
 });
 
 test('isSunk method returns "deth" when shipArray contains all 1s', () => {
   const testBoard = gameBoard();
+  testBoard.ships.destroyer.hit(0);
   testBoard.ships.destroyer.hit(1);
   testBoard.ships.destroyer.hit(2);
-  testBoard.ships.destroyer.hit(3);
   expect(testBoard.ships.destroyer.isSunk()).toBe('deth');
 });
 
-test('receiveAttack function detects correct index based on provided coordinates', () => {
+test('receiveAttack function targets correct boardArray index based on provided coordinates', () => {
   const testBoard = gameBoard();
-  expect(testBoard.receiveAttack(0, 2)).toEqual(20);
-})
+  testBoard.receiveAttack(0, 2);
+  expect(testBoard.boardArray[20][2]).toEqual(1);
+});
+
+test('receiveAttack function detects a ship at the specified coordinates', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
+  testBoard.receiveAttack(0, 2);
+  expect(testBoard.boardArray[20][3]).toEqual('destroyer');
+});
+
+test('hit function appropriately "damages" the detected ship in vertical orientation', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
+  testBoard.receiveAttack(0, 2);
+  expect(testBoard.ships.destroyer.shipArray).toEqual([0, 0, 1]);
+});
+
+test('hit function appropriately "damages" the detected ship in horizontal orientation', () => {
+  const testBoard = gameBoard();
+  testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
+  testBoard.receiveAttack(2, 0);
+  expect(testBoard.ships.destroyer.shipArray).toEqual([0, 0, 1]);
+});
 
 test('placeShip function adds ship information to indicated beginning index with vertical orientation', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
-  expect(testBoard.boardArray[0]).toEqual([0, 0, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[0]).toEqual([0, 0, 0, 'destroyer', 0]);
 });
 
 test('placeShip function adds ship information to all indexes it should occupy with vertical orientation (1)', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
-  expect(testBoard.boardArray[10]).toEqual([0, 1, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[10]).toEqual([0, 1, 0, 'destroyer', 1]);
 });
 
 test('placeShip function adds ship information to all indexes it should occupy with vertical orientation (2)', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 0, 0);
-  expect(testBoard.boardArray[20]).toEqual([0, 2, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[20]).toEqual([0, 2, 0, 'destroyer', 2]);
 });
 
 test('placeShip function adds ship information to indicated beginning index with horizontal orientation', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
-  expect(testBoard.boardArray[0]).toEqual([0, 0, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[0]).toEqual([0, 0, 0, 'destroyer', 0]);
 });
 
 test('placeShip function adds ship information to all indexes it should occupy with horizontal orientation (1)', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
-  expect(testBoard.boardArray[1]).toEqual([1, 0, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[1]).toEqual([1, 0, 0, 'destroyer', 1]);
 });
 
 test('placeShip function adds ship information to all indexes it should occupy with horizontal orientation(2)', () => {
   const testBoard = gameBoard();
   testBoard.placeShip(testBoard.ships.destroyer, 1, 0);
-  expect(testBoard.boardArray[2]).toEqual([2, 0, testBoard.ships.destroyer]);
+  expect(testBoard.boardArray[2]).toEqual([2, 0, 0, 'destroyer', 2]);
 });
 
 test('placeShip throws an error if a carrier would be placed out of bounds in vertical orientation', () => {

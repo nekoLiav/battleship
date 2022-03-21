d65b36d30f4f70e8c57af5a9ac5be13aef098cec
Liav <oneandonlyliav@gmail.com>
2022-03-21 09:05:43 -0400

    webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'source-map',
   module: {
     rules: [
       {

0672a27071dc0aa1f50e623677371409259f0fe9
Liav <oneandonlyliav@gmail.com>
2022-03-25 15:55:26 -0400

    fixed bug with attacking the same computer board space multiple times

--- src/scripts/game/playerAttack.js
+++ src/scripts/game/playerAttack.js
@@ -6,17 +6,15 @@ export default function playerAttack(e) {
   const computerSquares = document.getElementsByClassName('computer-square');
   if (e.target.parentNode.id === 'computer-board') {
     const dataIndex = e.target.getAttribute('data-i');
-    console.log(dataIndex);
-    console.log(computer.boardArray);
     if (computer.boardArray[dataIndex][2] === 0) {
       computer.receiveAttack(dataIndex);
+      updateStatus();
+      computerAttack();
       if (computer.boardArray[dataIndex].length > 3) {
         computerSquares[dataIndex].classList.add('hit');
       } else {
         computerSquares[dataIndex].classList.add('miss');
       }
     }
-    updateStatus();
-    computerAttack();
   }
 }

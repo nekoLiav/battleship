a2001351b47174b57241aecddff9132e5c03efc8
Liav <oneandonlyliav@gmail.com>
2022-03-27 11:27:13 -0400

    updated unit tests

--- src/scripts/tests/gameboard.test.js
+++ src/scripts/tests/gameboard.test.js
@@ -1,41 +1,33 @@
 /* eslint-disable no-undef */
-import gameBoard from '../factories/gameBoard';
-import ship from '../factories/ship';
-
-const testBoard = gameBoard();
-const testShip = ship(7, 'reallybigshipthatgoespewpew', 'billy');
+import { player } from '../game/players';
 
 test('board array contains 100 subarrays, each containing an x, y coordinate and a value indicating whether the space has been attacked (0 or 1)', () => {
-  expect(testBoard.board[13]).toEqual([3, 1, 0]);
+  expect(player.board[13]).toEqual([3, 1, 0]);
 });
 
 test('when ship is placed, it occupies the intended number of spaces horizontally', () => {
-  testBoard.placeShip(testShip, 0, 0, 0);
+  player.placeShip(player.ships.carrier, 0, 0, 0);
   expect(
-    testBoard.board[0][3] &&
-      testBoard.board[1][3] &&
-      testBoard.board[2][3] &&
-      testBoard.board[3][3] &&
-      testBoard.board[4][3] &&
-      testBoard.board[5][3] &&
-      testBoard.board[6][3]
-  ).toEqual('reallybigshipthatgoespewpew');
+    player.board[0][3] &&
+      player.board[1][3] &&
+      player.board[2][3] &&
+      player.board[3][3] &&
+      player.board[4][3]
+  ).toEqual('carrier');
 });
 
 test('when ship is placed, it occupies the intended number of spaces vertically', () => {
-  testBoard.placeShip(testShip, 1, 0, 0);
+  player.placeShip(player.ships.carrier, 1, 0, 0);
   expect(
-    testBoard.board[0][3] &&
-      testBoard.board[10][3] &&
-      testBoard.board[20][3] &&
-      testBoard.board[30][3] &&
-      testBoard.board[40][3] &&
-      testBoard.board[50][3] &&
-      testBoard.board[60][3]
-  ).toEqual('reallybigshipthatgoespewpew');
+    player.board[0][3] &&
+      player.board[10][3] &&
+      player.board[20][3] &&
+      player.board[30][3] &&
+      player.board[40][3]
+  ).toEqual('carrier');
 });
 
 test('board array responds as intended when attacked', () => {
-  testBoard.receiveAttack(13);
-  expect(testBoard.board[13][2]).toEqual(1);
+  player.receiveAttack(13);
+  expect(player.board[13][2]).toEqual(1);
 });
--- src/scripts/tests/ship.test.js
+++ src/scripts/tests/ship.test.js
@@ -1,29 +1,25 @@
 /* eslint-disable no-undef */
-import ship from '../factories/ship';
-
-const testShip = ship(7, 'reallybigshipgoespewpew', 'billy');
+import { player } from '../game/players';
 
 test('ship hits array is equivalent in size to its length', () => {
-  expect(testShip.hits.length).toEqual(testShip.length);
+  expect(player.ships.carrier.hits.length).toEqual(player.ships.carrier.length);
 });
 
 test('ship hits array responds as intended by hit function', () => {
-  testShip.hit(2);
-  expect(testShip.hits).toEqual([0, 0, 1, 0, 0, 0, 0]);
+  player.ships.carrier.hit(2);
+  expect(player.ships.carrier.hits).toEqual([0, 0, 1, 0, 0]);
 });
 
 test('ship sunk function returns false if ship hit', () => {
-  testShip.hit(2);
-  expect(testShip.sunk()).toEqual(false);
+  player.ships.carrier.hit(2);
+  expect(player.ships.carrier.sunk()).toEqual(false);
 });
 
 test('ship sunk function returns true if ship hits array is fully hit', () => {
-  testShip.hit(0);
-  testShip.hit(1);
-  testShip.hit(2);
-  testShip.hit(3);
-  testShip.hit(4);
-  testShip.hit(5);
-  testShip.hit(6);
-  expect(testShip.sunk()).toEqual(true);
+  player.ships.carrier.hit(0);
+  player.ships.carrier.hit(1);
+  player.ships.carrier.hit(2);
+  player.ships.carrier.hit(3);
+  player.ships.carrier.hit(4);
+  expect(player.ships.carrier.sunk()).toEqual(true);
 });

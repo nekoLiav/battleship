fa39288516377018e719d976186a50bcdabc8212
Liav <oneandonlyliav@gmail.com>
2022-03-28 14:55:14 -0400

    updated visual sunk detection and styling

--- src/scripts/ui/updateSunk.js
+++ src/scripts/ui/updateSunk.js
@@ -3,137 +3,85 @@ import { player, computer } from '../helpers/players';
 export default function updateSunk() {
   const playerSquare = document.querySelectorAll('.player-square');
   const computerSquare = document.querySelectorAll('.computer-square');
-  switch (true) {
-    case player.ships.carrier.sunk():
-      player.board.forEach((element) => {
-        if (element[3] === 'carrier') {
-          if (
-            !playerSquare[player.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            playerSquare[player.board.indexOf(element)].classList.add('sunk');
-          }
-        }
-      });
-    case player.ships.battleship.sunk():
-      player.board.forEach((element) => {
-        if (element[3] === 'battleship') {
-          if (
-            !playerSquare[player.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            playerSquare[player.board.indexOf(element)].classList.add('sunk');
-          }
-        }
-      });
-    case player.ships.destroyer.sunk():
-      player.board.forEach((element) => {
-        if (element[3] === 'destroyer') {
-          if (
-            !playerSquare[player.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            playerSquare[player.board.indexOf(element)].classList.add('sunk');
-          }
-        }
-      });
-    case player.ships.submarine.sunk():
-      player.board.forEach((element) => {
-        if (element[3] === 'submarine') {
-          if (
-            !playerSquare[player.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            playerSquare[player.board.indexOf(element)].classList.add('sunk');
-          }
-        }
-      });
-    case player.ships.patrolboat.sunk():
-      player.board.forEach((element) => {
-        if (element[3] === 'patrolboat') {
-          if (
-            !playerSquare[player.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            playerSquare[player.board.indexOf(element)].classList.add('sunk');
-          }
-        }
-      });
-    case computer.ships.carrier.sunk():
-      computer.board.forEach((element) => {
-        if (element[3] === 'carrier') {
-          if (
-            !computerSquare[computer.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            computerSquare[computer.board.indexOf(element)].classList.add(
-              'sunk'
-            );
-          }
-        }
-      });
-    case computer.ships.battleship.sunk():
-      computer.board.forEach((element) => {
-        if (element[3] === 'battleship') {
-          if (
-            !computerSquare[computer.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            computerSquare[computer.board.indexOf(element)].classList.add(
-              'sunk'
-            );
-          }
-        }
-      });
-    case computer.ships.destroyer.sunk():
-      computer.board.forEach((element) => {
-        if (element[3] === 'destroyer') {
-          if (
-            !computerSquare[computer.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            computerSquare[computer.board.indexOf(element)].classList.add(
-              'sunk'
-            );
-          }
-        }
-      });
-    case computer.ships.submarine.sunk():
-      computer.board.forEach((element) => {
-        if (element[3] === 'submarine') {
-          if (
-            !computerSquare[computer.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            computerSquare[computer.board.indexOf(element)].classList.add(
-              'sunk'
-            );
-          }
-        }
-      });
-    case computer.ships.patrolboat.sunk():
-      computer.board.forEach((element) => {
-        if (element[3] === 'patrolboat') {
-          if (
-            !computerSquare[computer.board.indexOf(element)].classList.contains(
-              'sunk'
-            )
-          ) {
-            computerSquare[computer.board.indexOf(element)].classList.add(
-              'sunk'
-            );
-          }
-        }
-      });
-      break;
+
+  if (player.ships.carrier.sunk()) {
+    player.board.forEach((element) => {
+      if (element[3] === 'carrier') {
+        playerSquare[player.board.indexOf(element)].classList.remove('hit');
+        playerSquare[player.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (player.ships.battleship.sunk()) {
+    player.board.forEach((element) => {
+      if (element[3] === 'battleship') {
+        playerSquare[player.board.indexOf(element)].classList.remove('hit');
+        playerSquare[player.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (player.ships.destroyer.sunk()) {
+    player.board.forEach((element) => {
+      if (element[3] === 'destroyer') {
+        playerSquare[player.board.indexOf(element)].classList.remove('hit');
+        playerSquare[player.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (player.ships.submarine.sunk()) {
+    player.board.forEach((element) => {
+      if (element[3] === 'submarine') {
+        playerSquare[player.board.indexOf(element)].classList.remove('hit');
+        playerSquare[player.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (player.ships.patrolboat.sunk()) {
+    player.board.forEach((element) => {
+      if (element[3] === 'patrolboat') {
+        playerSquare[player.board.indexOf(element)].classList.remove('hit');
+        playerSquare[player.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (computer.ships.carrier.sunk()) {
+    computer.board.forEach((element) => {
+      if (element[3] === 'carrier') {
+        computerSquare[computer.board.indexOf(element)].classList.remove('hit');
+        computerSquare[computer.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (computer.ships.battleship.sunk()) {
+    computer.board.forEach((element) => {
+      if (element[3] === 'battleship') {
+        computerSquare[computer.board.indexOf(element)].classList.remove('hit');
+        computerSquare[computer.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (computer.ships.destroyer.sunk()) {
+    computer.board.forEach((element) => {
+      if (element[3] === 'destroyer') {
+        computerSquare[computer.board.indexOf(element)].classList.remove('hit');
+        computerSquare[computer.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (computer.ships.submarine.sunk()) {
+    computer.board.forEach((element) => {
+      if (element[3] === 'submarine') {
+        computerSquare[computer.board.indexOf(element)].classList.remove('hit');
+        computerSquare[computer.board.indexOf(element)].classList.add('sunk');
+      }
+    });
+  }
+  if (computer.ships.patrolboat.sunk()) {
+    computer.board.forEach((element) => {
+      if (element[3] === 'patrolboat') {
+        computerSquare[computer.board.indexOf(element)].classList.remove('hit');
+        computerSquare[computer.board.indexOf(element)].classList.add('sunk');
+      }
+    });
   }
 }

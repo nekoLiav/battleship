5a3b6847af2f8a0d5d86c0c3635eed1a952dbfd9
Liav <oneandonlyliav@gmail.com>
2022-10-19 19:19:24 -0400

    add start game indicator text

--- src/scripts/ui/checkStart.js
+++ src/scripts/ui/checkStart.js
@@ -4,5 +4,8 @@ export default function checkStart() {
   if (player.placedShips.length === 5) {
     document.getElementById('direction-button').style.display = 'none';
     document.getElementById('start-button').style.display = 'block';
+    document.getElementById(
+      'player-status'
+    ).innerHTML = `Press 'START GAME' to play!`;
   }
 }
